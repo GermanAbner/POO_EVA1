ffcd1a50d0b28e1f6976f69bf9340749b4c30c34
GermanAbnerRangelLuna <miriamluna.q@gmail.com>
2026-02-04 18:46:01 -0600

    EVA1_Practica3

---
+++ EVA1_Practica3/src/OtroPaquete/Persona.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package OtroPaquete;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Persona {
+    //ATRIBUTOS
+    String  nombre; //default --> SOLO SERA VISIBLE DENTRO DEL PROPIO PAQUETE DE LA CLASE
+    public String apellidos; // public -->  VISIBLE EN TODOS LOS PAQUETES DEL PROGRAMA
+    public int edad; // private --> SIMPLEMENTE SERA VISIBLE A LO LARGO DE LA CLASE NO DEL PROGRAMA OKK??¡
+}
---
+++ EVA1_Practica3/src/eva1_practica3/EVA1_Practica3.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_practica3;
+
+import OtroPaquete.Persona;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_Practica3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Persona persona = new Persona(); //INSTANCIACION --> CREAR EL OBJETO
+        persona.apellidos
+    }
+    
+}
